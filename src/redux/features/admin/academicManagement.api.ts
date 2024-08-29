58d8abeeba24aa6e2b9f1e894547a41ae20105bf
MohammadJewelRana <js.rana0326@gmail.com>
2024-08-29 11:13:30 +0600

    create academic semester and display it

--- src/redux/features/admin/academicManagement.api.ts
+++ src/redux/features/admin/academicManagement.api.ts
@@ -2,33 +2,34 @@
  
   
   import { TAcademicDepartment, TAcademicFaculty, TAcademicSemester } from '../../../types/academicManagement.type';
+import { TQueryParam, TResponseRedux } from '../../../types/global';
 import { baseApi } from '../../api/baseApi';
   
   const academicManagementApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
-    //   getAllSemesters: builder.query({
-    //     query: (args) => {
-    //       const params = new URLSearchParams();
+      getAllSemesters: builder.query({
+        query: (args) => {
+          const params = new URLSearchParams();
   
-    //       if (args) {
-    //         args.forEach((item: TQueryParam) => {
-    //           params.append(item.name, item.value as string);
-    //         });
-    //       }
+          if (args) {
+            args.forEach((item: TQueryParam) => {
+              params.append(item.name, item.value as string);
+            });
+          }
   
-    //       return {
-    //         url: '/academic-semesters',
-    //         method: 'GET',
-    //         params: params,
-    //       };
-    //     },
-    //     transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
-    //       return {
-    //         data: response.data,
-    //         meta: response.meta,
-    //       };
-    //     },
-    //   }),
+          return {
+            url: '/academic-semesters',
+            method: 'GET',
+            params: params,
+          };
+        },
+        transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
+          return {
+            data: response.data,
+            meta: response.meta,
+          };
+        },
+      }),
 
 
       addAcademicSemester: builder.mutation({
